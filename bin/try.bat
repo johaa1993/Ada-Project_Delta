f8eb2f07735835a2d6c4e822cc053efb7980c010
Johan <johaa1993@gmail.com>
2016-03-30 21:50:16 +0200

    Misc

--- bin/try.bat
+++ bin/try.bat
@@ -1,7 +1,7 @@
 @cls
-@echo ==========Normalize==========
-@normalize.exe -mm db.csv -o dbn.csv
-@normalize.exe -mm db.csv -data s.csv -o sn.csv
+::@echo ==========Normalize==========
+::@normalize.exe -mm db.csv -o dbn.csv
+::@normalize.exe -mm db.csv -data s.csv -o sn.csv
 
 
 ::@echo ==========Informaion==========
@@ -9,11 +9,17 @@
 ::@info.exe db.csv
 ::@echo:
 
-@echo ==========Distance==========
-@echo:
+::@echo ==========Distance==========
+::@echo:
 ::@distance.exe -db dbn.csv -s sn.csv -d manhattan -w 1.0 1.0
-@distance.exe -db dbn.csv -s sn.csv -d manhattan -w 1.0 1.0 -o d.csv
+::@distance.exe -db dbn.csv -s sn.csv -d manhattan -w 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.0 -o d.csv
 
-@echo ==========K-Sort==========
-@echo:
-@ksort.exe -d d.csv -k 4 -csv p.csv
+::@echo ==========K-Sort==========
+::@echo:
+
+
+@distance.exe -db dbn.csv -s sn.csv -d canberra -w 1.0 1.0 1.0 1.0 1.0 1.0 1.0 0.0 -o d.csv
+@ksort.exe -d d.csv -k 10 -put
+
+@distance.exe -db dbn.csv -s sn.csv -d canberra -w 1.0 1.0 1.0 1.0 0.0 1.0 1.0 0.0 -o d.csv
+@ksort.exe -d d.csv -k 10 -put
